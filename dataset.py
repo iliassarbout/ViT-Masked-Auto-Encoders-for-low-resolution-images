83c09eb5de2ddf7d54cfb0c660ef1bcfe521f6f6
iliassarbout <dexfray17@gmail.com>
2022-10-16 23:39:17 +0200

    up

---
+++ dataset.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class DATA(Dataset):
+    def __init__(self, img_list, transform = None, mean = None,std = None):
+        self.img_list = img_list
+        self.transform = transform
+        self.mean = mean
+        self.std = std
+
+        if(self.mean is not None and self.std is not None):
+            self.normalize = transforms.Normalize(mean=self.mean,
+                         std=self.std)
+        else:
+            self.normalize = None
+        
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        img_path = self.img_list[idx]
+        img = torch.load(img_path)
+        
+        #img = img.unsqueeze(diml=0) #add a dimension (same as img = img[None,:]) #not necssary as dataloder will create batch dimension
+        if self.normalize is not None:
+            img = self.normalize(img)
+        if self.transform is not None:
+            img = self.transform(img)
+        #img = torch.einsum('hwc->chw', img) #switch channel position
+
+        return img,idx
